de8c4ba27c5cb525c177ee3c75a7433b9da01c19
Daniel Taherkhanian <daniel.taherkhanian@elev.ga.ntig.se>
2023-04-27 20:57:25 +0200

    V 1.0

--- Slutprojekt/GameHealth.cs
+++ Slutprojekt/GameHealth.cs
@@ -3,6 +3,7 @@ using System;
 public class GameHealth
 {
     public static int playerHealth = 120;
+    public static int enemyHealth;
     public static int enemyEasyHealth = 20;
     public static int enemyMediumHealth = 40;
     public static int enemyHardHealth = 60;
